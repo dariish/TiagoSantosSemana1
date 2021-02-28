8b7e7952e770f640dac7467bc735ab0c43e88bf8
Tiago Santos <darish26@hotmail.com>
2021-02-28 21:26:34 +0000

    Initial commit

---
+++ app/src/main/java/ipvc/estg/TiagoSantos/MainActivity.kt
@@ -0,0 +1,13 @@
+package ipvc.estg.TiagoSantos
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
+    }
+}
